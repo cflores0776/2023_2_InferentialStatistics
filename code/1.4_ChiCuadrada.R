415426b58aa426d9b0ff83bc05636126d569cc1a
Carlos A. Flores S <137957506+cflores0776@users.noreply.github.com>
2023-08-22 20:13:16 -0700

    Una linea de datos para contrastar resultado de pvalue

--- code/1.4_ChiCuadrada.R
+++ code/1.4_ChiCuadrada.R
@@ -1,5 +1,6 @@
 # Crear una tabla de contingencia con los datos ficticios
-datos <- matrix(c(50, 30, 40, 25), nrow = 2, byrow = TRUE)
+#datos <- matrix(c(50, 30, 40, 25), nrow = 2, byrow = TRUE)
+datos <- matrix(c(10, 1, 1, 10), nrow = 2, byrow = TRUE)
 rownames(datos) <- c("Comida Rápida", "Comida Saludable")
 colnames(datos) <- c("Lunes/Martes", "Miércoles/Jueves")
 
